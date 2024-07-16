0d7866371b97e5b9f93ddacfffecce5e4ad210a9
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-07-16 07:41:07 +0200

    setting base Url

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: "http://127.0.0.1:5500/",
     setupNodeEvents() {
       // implement node event listeners here
     },
