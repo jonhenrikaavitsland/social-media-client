c09d4bebe574a6075a7b3349f5a3fbcd9643d2ee
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-07-17 10:29:59 +0200

    e2e testing login function

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,44 @@
+/// <reference types="cypress" />
+
+describe("login form", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.wait(500);
+  });
+
+  it("should log in with valid credentials", () => {
+    cy.get(`[data-cy="loginFormBtn"]`).click();
+    cy.wait(500);
+    cy.get(`[data-cy="emailInput"]`).click();
+    cy.get(`[data-cy="emailInput"]`).type("testcypress@stud.noroff.no");
+    cy.get(`[data-cy="passwordInput"]`).click();
+    cy.get(`[data-cy="passwordInput"]`).type(`123456789{enter}`);
+    cy.wait(1000);
+    cy.get(".profile-actions h4").should("have.text", "testcypress");
+    cy.get(`[data-auth="logout"]`).click();
+    cy.wait(500);
+    cy.location("pathname").should("eq", "/");
+    cy.get(`[data-cy="loginFormBtn"]`).click();
+    cy.wait(500);
+    cy.get(`[data-cy="emailInput"]`).click();
+    cy.get(`[data-cy="emailInput"]`).type("testcypress@stud.noroff.no");
+    cy.get(`[data-cy="passwordInput"]`).click();
+    cy.get(`[data-cy="passwordInput"]`).type(`123456789`);
+    cy.get(`[data-cy="login-btn"]`).click();
+    cy.wait(1000);
+    cy.get(".profile-actions h4").should("have.text", "testcypress");
+  });
+
+  it("should not submit the login form when provided with invalid credentials and user is shown a message", () => {
+    cy.get(`[data-cy="loginFormBtn"]`).click();
+    cy.wait(500);
+    cy.get(`[data-cy="emailInput"]`).click();
+    cy.get(`[data-cy="emailInput"]`).type("invalidtestemail@stud.noroff.no");
+    cy.get(`[data-cy="passwordInput"]`).click();
+    cy.get(`[data-cy="passwordInput"]`).type(`invalidpassword{enter}`);
+    cy.wait(1000);
+    cy.on("window:alert", () => {
+      expect(true).to.be.true;
+    });
+  });
+});
