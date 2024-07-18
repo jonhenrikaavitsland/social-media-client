328e5a7669d6452a260c878b301acd9ee4b53738
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-07-18 09:46:55 +0200

    cleared comments

--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -7,11 +7,6 @@ describe("logout function", () => {
     cy.wait(500);
     cy.get(`[data-cy="emailInput"]`).click();
     cy.getCredentials();
-    // cy.get("@credentials").then((user) => {
-    //   cy.get(`[data-cy="emailInput"]`).type(`${user.invalidEmail}`);
-    //   cy.get(`[data-cy="passwordInput"]`).click();
-    //   cy.get(`[data-cy="passwordInput"]`).type(`${user.password}{enter}`);
-    // });
     cy.wait("@loginReq");
     cy.window().its("localStorage.token").should("exist");
     cy.window().its("localStorage.profile").should("exist");
