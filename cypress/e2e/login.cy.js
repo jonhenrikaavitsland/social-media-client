7bc6b0bbdb9b082d74be945c48af768b2434c241
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-07-18 09:14:12 +0200

    updated e2e tests to be more effecient

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,47 +1,54 @@
 /// <reference types="cypress" />
 
-import { apiUrl } from "../../src/js/api/constants";
-
 describe("login form", () => {
   beforeEach(() => {
+    cy.fixture("credentials.json").as("credentials");
     cy.visitPage();
   });
 
   it("should log in with valid credentials", () => {
-    cy.interceptRequest(200);
+    cy.interceptSuccess().as("loginReq");
     cy.get(`[data-cy="loginFormBtn"]`).click();
     cy.wait(500);
     cy.get(`[data-cy="emailInput"]`).click();
-    cy.get(`[data-cy="emailInput"]`).type("testcypress@stud.noroff.no");
-    cy.get(`[data-cy="passwordInput"]`).click();
-    cy.get(`[data-cy="passwordInput"]`).type(`123456789{enter}`);
-    cy.wait(1000);
-    cy.get(".profile-actions h4").should("have.text", "testcypress");
-    cy.get(`[data-auth="logout"]`).click();
+    cy.getCredentials();
+    cy.wait("@loginReq");
+    cy.window().its("localStorage.token").should("exist");
+    cy.window().its("localStorage.profile").should("exist");
+    cy.logoutFn();
     cy.wait(500);
-    cy.location("pathname").should("eq", "/");
     cy.get(`[data-cy="loginFormBtn"]`).click();
     cy.wait(500);
     cy.get(`[data-cy="emailInput"]`).click();
-    cy.get(`[data-cy="emailInput"]`).type("testcypress@stud.noroff.no");
-    cy.get(`[data-cy="passwordInput"]`).click();
-    cy.get(`[data-cy="passwordInput"]`).type(`123456789`);
+    cy.get("@credentials").then((user) => {
+      cy.get(`[data-cy="emailInput"]`).type(`${user.email}`);
+      cy.get(`[data-cy="passwordInput"]`).click();
+      cy.get(`[data-cy="passwordInput"]`).type(`${user.password}`);
+    });
     cy.get(`[data-cy="login-btn"]`).click();
-    cy.wait(1000);
-    cy.get(".profile-actions h4").should("have.text", "testcypress");
+    cy.wait("@loginReq");
+    cy.window().its("localStorage.token").should("exist");
+    cy.window().its("localStorage.profile").should("exist");
   });
 
   it("should not submit the login form when provided with invalid credentials and user is shown a message", () => {
-    cy.interceptRequest(401);
+    cy.intercept(
+      "POST",
+      "https://nf-api.onrender.com/api/v1/social/auth/login",
+      { statusCode: 401, body: { message: "Invalid email or password" } },
+    ).as("failedLogin");
     cy.get(`[data-cy="loginFormBtn"]`).click();
     cy.wait(500);
     cy.get(`[data-cy="emailInput"]`).click();
-    cy.get(`[data-cy="emailInput"]`).type("invalidtestemail@stud.noroff.no");
-    cy.get(`[data-cy="passwordInput"]`).click();
-    cy.get(`[data-cy="passwordInput"]`).type(`invalidpassword{enter}`);
-    cy.wait(1000);
+    cy.get("@credentials").then((user) => {
+      cy.get(`[data-cy="emailInput"]`).type(`${user.invalidEmail}`);
+      cy.get(`[data-cy="passwordInput"]`).click();
+      cy.get(`[data-cy="passwordInput"]`).type(`${user.password}{enter}`);
+    });
+    cy.wait("@failedLogin");
     cy.on("window:alert", () => {
       expect(true).to.be.true;
     });
+    cy.location("pathname").should("eq", "/");
   });
 });
--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -1,17 +1,20 @@
 describe("logout function", () => {
   it("should log the user out when clicking the logout button", () => {
-    cy.interceptRequest(200);
+    cy.fixture("credentials.json").as("credentials");
+    cy.interceptSuccess().as("loginReq");
     cy.visitPage();
     cy.get(`[data-cy="loginFormBtn"]`).click();
     cy.wait(500);
     cy.get(`[data-cy="emailInput"]`).click();
-    cy.get(`[data-cy="emailInput"]`).type("testcypress@stud.noroff.no");
-    cy.get(`[data-cy="passwordInput"]`).click();
-    cy.get(`[data-cy="passwordInput"]`).type(`123456789{enter}`);
-    cy.wait(1000);
-    cy.get(".profile-actions h4").should("have.text", "testcypress");
-    cy.get(`[data-auth="logout"]`).click();
-    cy.wait(500);
-    cy.location("pathname").should("eq", "/");
+    cy.getCredentials();
+    // cy.get("@credentials").then((user) => {
+    //   cy.get(`[data-cy="emailInput"]`).type(`${user.invalidEmail}`);
+    //   cy.get(`[data-cy="passwordInput"]`).click();
+    //   cy.get(`[data-cy="passwordInput"]`).type(`${user.password}{enter}`);
+    // });
+    cy.wait("@loginReq");
+    cy.window().its("localStorage.token").should("exist");
+    cy.window().its("localStorage.profile").should("exist");
+    cy.logoutFn();
   });
 });
